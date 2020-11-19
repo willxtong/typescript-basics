09923a16841c2f4407b54a82db08a94188ec3a74
William Tong <willxtong@gmail.com>
2020-11-18 20:29:35 -0500

    sort linked list

---
+++ mini-apps/sort/src/LinkedList.ts
@@ -0,0 +1,62 @@
+import { Sortable } from './Sortable';
+
+class Node {
+  next: Node | null = null;
+  constructor(public data: number) {}
+}
+
+export class LinkedList implements Sortable {
+  head: Node | null = null;
+  tail: Node | null = null;
+  length: number = 0;
+
+  add(data: number): void {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      this.length = 1;
+    } else if (this.tail) {
+      this.tail.next = node;
+      this.tail = node;
+      this.length++;
+    }
+  }
+
+  at(index: number): Node {
+    if (!this.head) throw new Error('Index out of bounds');
+    let count = 0;
+    let curr: Node | null = this.head;
+    while (curr) {
+      if (count === index) return curr;
+      count++;
+      curr = curr.next;
+    }
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) throw new Error('List is empty');
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    if (!this.head) throw new Error('List is empty');
+
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftData = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftData;
+  }
+
+  print(): void {
+    if (!this.head) return;
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- mini-apps/sort/src/index.ts
+++ mini-apps/sort/src/index.ts
@@ -1,13 +1,23 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 // const nums = new NumberCollection([0, 10, -1, 5]);
 // const sortNums = new Sorter(nums);
 // sortNums.sort();
 // console.log(nums.data);
 
-const str = new CharacterCollection('XaybaO');
-const sortStr = new Sorter(str);
-sortStr.sort();
-console.log(str.data);
+// const str = new CharacterCollection('XaybaO');
+// const sortStr = new Sorter(str);
+// sortStr.sort();
+// console.log(str.data);
+
+const linkedList = new LinkedList();
+linkedList.add(5);
+linkedList.add(3);
+linkedList.add(8);
+linkedList.add(-1);
+const sortLL = new Sorter(linkedList);
+sortLL.sort();
+linkedList.print();
